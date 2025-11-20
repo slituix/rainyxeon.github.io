1678eb529c2c391deff590b84ce533873afca0c1
slituix <saksham.mac@icloud.com>
2025-11-20 13:10:57 +0000

    Deploying to gh-pages from @ slituix/rainyxeon.github.io@4cc21bb07f8fcdf1cdf2ec9bb7af807f6a305b6d 🚀

---
+++ terminal/index-4nqmzzjz.js
@@ -0,0 +1,100 @@
+var B=document.querySelector(":root"),v=localStorage.getItem("/settings/effects");if(!v)localStorage.setItem("/settings/effects","yes");else if(v=="no")B.style.setProperty("--terminal-placeholder-color","#c9c9c9"),B.style.setProperty("--terminal-color","#ffffff"),B.style.setProperty("--text-shadow","0"),document.getElementById("term_screen").removeAttribute("effect");function S(){let e=!1;return function(t){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(t)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(t.substr(0,4)))e=!0}(navigator.userAgent||navigator.vendor||window.opera),e}var O=S();if(O)document.getElementById("term_screen").innerHTML=`<pre style="display: block; width: 100%;">
+If you see this screen, that means, you're viewing this portfolio in the phone
+Currently my portfolio not support phone input!
+Sorry for inconvenient, you can view my profile via <a href="../" >main page</a>
+
+Catch me on:
+  - <a href="https://discord.com/users/898728768791789628" target="_blank">Discord (rainyxeon)</a>
+  - <a href="mailto:rainyxeon@gmail.com">Email</a>
+  - <a href="https://x.com/RainyXeon" target="_blank">X (Twitter)</a>
+
+Some of my famous projects:
+  - <a href="https://github.com/LunaticSea" target="_blank">LunaticSea Ecosystem</a>
+  - <a href="https://github.com/RainyXeon/ByteBlaze" target="_blank">ByteBlaze</a>
+  - <a href="https://github.com/RainyXeon/Rainlink" target="_blank">Rainlink</a>
+  - <a href="https://github.com/RainyXeon/Cylane" target="_blank">Cylane</a>
+
+“I remember you was conflicted
+
+Misusing your influence
+
+Sometimes I did the same
+
+Abusing my power, full of resentment
+
+Resentment that turned into a deep depression
+
+Found myself screaming in the hotel room
+
+I didn’t wanna self destruct
+
+The evils of Lucy was all around me
+
+So I went running for answers
+
+Until I came home
+
+But that didn’t stop survivor’s guilt
+
+Going back and forth trying to convince myself the stripes I earned
+
+Or maybe how A-1 my foundation was
+
+But while my loved ones was fighting the continuous war back in the city, I was entering a new one
+
+A war that was based on apartheid and discrimination
+
+Made me wanna go back to the city and tell the homies what I learned
+
+The word was respect
+
+Just because you wore a different gang color than mine's
+
+Doesn’t mean I can’t respect you as a black man
+
+Forgetting all the pain and hurt we caused each other in these streets
+
+If I respect you, we unify and stop the enemy from killing us
+
+But I don’t know, I’m no mortal man, maybe I’m just another nigga”
+
+― Kendrick Lamar, To Pimp a Butterfly
+</pre>`;var s={name:"HELP",desc:"List all command, usage and its description",usage:null,run:(e,t)=>{let n=[s.template("Avaliable commands:"),s.template()],a=Object.values(t);for(let r of a){if(r.hidden)continue;if(n.push(s.template(r.name)),n.push(s.template(`Description: ${r.desc}`,40)),r.usage!==null)n.push(s.template(`Usage: ${r.usage}`,40));n.push(s.template())}document.getElementById("term_screen").innerHTML+=n.join(`
+`)},template:(e,t)=>{if(!e&&e==null)return'<div style="display: block; height: 20px;"></div>';else return`<div style="${t?`padding-left: ${t}px`:""}">${e}</div>`}},k=s;var I=(e)=>Number.isFinite(e)?e:0;function j(e){return{days:Math.trunc(e/86400000),hours:Math.trunc(e/3600000%24),minutes:Math.trunc(e/60000%60),seconds:Math.trunc(e/1000%60),milliseconds:Math.trunc(e%1000),microseconds:Math.trunc(I(e*1000)%1000),nanoseconds:Math.trunc(I(e*1e6)%1000)}}function X(e){return{days:e/86400000n,hours:e/3600000n%24n,minutes:e/60000n%60n,seconds:e/1000n%60n,milliseconds:e%1000n,microseconds:0n,nanoseconds:0n}}function b(e){switch(typeof e){case"number":{if(Number.isFinite(e))return j(e);break}case"bigint":return X(e)}throw TypeError("Expected a finite number or bigint")}var N=(e)=>e===0||e===0n,z=(e,t)=>t===1||t===1n?e:`${e}s`,$=0.0000001,U=24n*60n*60n*1000n;function y(e,t){let n=typeof e==="bigint";if(!n&&!Number.isFinite(e))throw TypeError("Expected a finite number or bigint");t={...t};let a=e<0?"-":"";if(e=e<0?-e:e,t.colonNotation)t.compact=!1,t.formatSubMilliseconds=!1,t.separateMilliseconds=!1,t.verbose=!1;if(t.compact)t.unitCount=1,t.secondsDecimalDigits=0,t.millisecondsDecimalDigits=0;let r=[],R=(A,c)=>{let m=Math.floor(A*10**c+$);return(Math.round(m)/10**c).toFixed(c)},i=(A,c,m,o)=>{if((r.length===0||!t.colonNotation)&&N(A)&&!(t.colonNotation&&m==="m"))return;if(o??=String(A),t.colonNotation){let l=o.includes(".")?o.split(".")[0].length:o.length,f=r.length>0?2:1;o="0".repeat(Math.max(0,f-l))+o}else o+=t.verbose?" "+z(c,A):m;r.push(o)},u=b(e),p=BigInt(u.days);if(t.hideYearAndDays)i(BigInt(p)*24n+BigInt(u.hours),"hour","h");else{if(t.hideYear)i(p,"day","d");else i(p/365n,"year","y"),i(p%365n,"day","d");i(Number(u.hours),"hour","h")}if(i(Number(u.minutes),"minute","m"),!t.hideSeconds)if(t.separateMilliseconds||t.formatSubMilliseconds||!t.colonNotation&&e<1000){let A=Number(u.seconds),c=Number(u.milliseconds),m=Number(u.microseconds),o=Number(u.nanoseconds);if(i(A,"second","s"),t.formatSubMilliseconds)i(c,"millisecond","ms"),i(m,"microsecond","µs"),i(o,"nanosecond","ns");else{let l=c+m/1000+o/1e6,f=typeof t.millisecondsDecimalDigits==="number"?t.millisecondsDecimalDigits:0,D=l>=1?Math.round(l):Math.ceil(l),w=f?l.toFixed(f):D;i(Number.parseFloat(w),"millisecond","ms",w)}}else{let A=(n?Number(e%U):e)/1000%60,c=typeof t.secondsDecimalDigits==="number"?t.secondsDecimalDigits:1,m=R(A,c),o=t.keepDecimalsOnWholeSeconds?m:m.replace(/\.0+$/,"");i(Number.parseFloat(o),"second","s",o)}if(r.length===0)return a+"0"+(t.verbose?" milliseconds":"ms");let T=t.colonNotation?":":" ";if(typeof t.unitCount==="number")r=r.slice(0,Math.max(t.unitCount,1));return a+r.join(T)}var F={name:"INFO",desc:"My current infomation",usage:null,run:(e,t)=>{let n=['<a href="https://github.com/LunaticSea" target="_blank">LunaticSea Ecosystem</a>','<a href="https://github.com/RainyXeon/ByteBlaze" target="_blank">ByteBlaze</a>','<a href="https://github.com/RainyXeon/Rainlink" target="_blank">Rainlink</a>','<a href="https://github.com/RainyXeon/Cylane" target="_blank">Cylane</a>'],a=['<a href="https://github.com/RainyXeon" target="_blank">Github</a>','<a href="https://discord.com/users/898728768791789628" target="_blank">Discord (rainyxeon)</a>','<a href="mailto:xeondev@xeondex.onmicrossoft.com">Email</a>','<a href="https://x.com/RainyXeon" target="_blank">X (Twitter)</a>'];document.getElementById("term_screen").innerHTML+=`<pre>
+             .^.          .~.              | RainyXeon (Arisu: Rain)
+            ^G^7?7!^..^!7?7^B.             | ----------------------------------------------------------------
+           .G.Y7775&57777!?J:B.            | Description: A full stack developer from Vietnam
+          .#J57:J7Y#57777J.Y?^G            | Online: ${y(Date.now()-1210872600000)}
+      .^!7J?G#JB:!Y#5777!:B.5!?&J!^:       | Language (Senior): JavaScript, TypeScript, Lua
+   .5777?77777BPB5Y#57!^Y?:G~&G5J?7!!P.    | Language (Amateur): C#, C++, C, Java  
+    B:^B.!J7!77JG7^.     5YP&??J~.B:~P     | Database: MongoDB, MySQL, PostgreSQL, Redis
+    .B.Y7^B 5Y.           BJYB #!JP^B      | Framework: ExpressJS, ReactJS, NestJS
+     :B.P~!5.G.            GBY&J#PP#.      | Projects: ${n.join(", ")}
+     .#PP#J&YBG            .G.5!~P.B:      | Best at: Backend, Frontend, DevOps and A bit of Mobile App
+     B^PJ!# BYJB           .Y5 B^7Y.B.     | Years of Experience: ${y(Date.now()-1588294800000)}
+    P~:B.~J??&PY5     .^7GJ77!7J!.B^:B     | Contacts: ${a.join(", ")}
+   .P!!7?J5G&~G:?Y^!75#Y5BPB77777?7775.    | You can call me:
+      :^!J&?!5.B:!7775#Y!:BJ#G?J7!^.       |  - rainy
+           G^?Y.J77775#Y7J:75J#.           |  - ArisuRain
+           .B:J?!77775&5777Y.G.            |  - Chuvoso
+            .B^7?7!^..^!7?7^G^             |  - Ame-san
+             .~.          .^.              |
+</pre>`}},E=F;var Z={name:"DIR",desc:"List all file",usage:null,run:(e,t)=>{document.getElementById("term_screen").innerHTML+="<pre>RAINY.exe</pre>"}},x=Z;var K={name:"CLS",desc:"Clear all screen (short)",usage:null,run:(e,t)=>{document.getElementById("term_screen").innerHTML=""}},C=K;var q={name:"RAINY",desc:"List all file",usage:null,hidden:!0,run:(e,t)=>{document.getElementById("term_screen").innerHTML+=`<pre>
+    Hi, if you see this file and execute it then too bad for you.
+    This file doesn't contain any infomation except text.
+    To know more about me, please use 'info' command :)
+
+    Peace,
+    RainyXeon</pre>`}},L=q;var V={name:"AUDIO",desc:"Enable or disable audio",usage:"AUDIO [ENABLE or DISABLE]",run:(e,t)=>{let n=["ENABLE","DISABLE"],[,a]=e.target.value.split(" ");if(!n.includes(a))return document.getElementById("term_screen").innerHTML+="<div>Invalid arg, please use ENABLE and DISABLE only</div>";switch(a){case"ENABLE":localStorage.setItem("/settings/audio/beep","yes"),new Audio("assets/audio/beep.mp3").play();break;case"DISABLE":localStorage.setItem("/settings/audio/beep","no");break}},template:(e,t)=>{if(!e&&e==null)return'<div style="display: block; height: 20px;"></div>';return`<div style="${t?`padding-left: ${t}px`:""}">${e}</div>`}},G=V;var W={name:"SYSINFO",desc:"Infomation for this Rainy's DOS",usage:null,run:(e,t)=>{document.getElementById("term_screen").innerHTML+=`
+      <div>rDOS (Rainy's DOS)</div>
+      <div>Version 1.0.3 Copyright (C) DeepInRain Production. All rights reserved.</div>
+      <div style="display: block; height: 20px;"></div>
+    `}},_=W;function ee(e){let t=window.atob(e);return new TextDecoder().decode(Uint8Array.from(t,(n)=>n.codePointAt(0)))}function te(e){let t=new TextEncoder().encode(e),n=Array.from(t,(a)=>String.fromCodePoint(a)).join("");return window.btoa(n)}var ne={name:"BASE64",desc:"Encode or decode string",usage:"BASE64 [ENCODE or DECODE] [string]",run:(e,t)=>{let n=["ENCODE","DECODE"],[,a]=e.target.value.split(" "),r=e.target.value.split(" ").slice(2).join(" ");if(!n.includes(a))return document.getElementById("term_screen").innerHTML+="<div>Invalid arg, please use ENCODE and DECODE only</div>";if(!r)return document.getElementById("term_screen").innerHTML+="<div>Missing input</div>";if(a=="ENCODE")return document.getElementById("term_screen").innerHTML+=`<div>${te(r)}</div>`;if(a=="DECODE")return document.getElementById("term_screen").innerHTML+=`<div>${ee(r)}</div>`}},H=ne;var ae={name:"BACK",desc:"Go back to portfolio",usage:null,run:(e,t)=>{return window.open("../","_self")}},P=ae;var Y="PABwAHIAZQA+AAoATwBoACAAbgBvACwAIAB5AG8AdQAnAHYAZQAgAHAAcgBlAHMAcwBlAGQAIABlAG4AdABlAHIAIAB0AG8AbwAgAG0AYQBuAHkAIAB0AGkAbQBlAHMAIAB3AGkAdABoAG8AdQB0ACAAYQBuAHkAIABjAG8AbQBtAGEAbgBkAAoASQAgAHQAaABpAG4AawAgAHkAbwB1ACAAbQBpAGcAaAB0ACAAYgBlACAAdQBwAHMAZQB0ACAAYQBiAG8AdQB0ACAAcwBvAG0AZQB0AGgAaQBuAGcACgBTAG8AIABJACcAbQAgAGwAZQBhAHYAaQBuAGcAIAB0AGgAZQAgAGMAYQB0ACAAaABlAHIAZQAgAHQAbwAgAGMAbwBtAGYAbwByAHQAIAB5AG8AdQAKAFAAbABlAGEAcwBlACAAcABlAHQAIABpAHQAIABhAG4AZAAgAGwAbwBvAGsAIABhAHQAIABpAHQAIAB3AGkAdABoACAAbABvAHYAaQBuAGcAIABlAHkAZQBzACEACgAKAAAoIAAP/2wAATAKAAj/n/9k/yAAYf8gABf/CgAAKCAAbAABMJ7/IAB+AP0wCgAEIAQgWDBXMGYAXwAsACAAKQDOMAoAPAAvAHAAcgBlAD4A";var M=new Audio("../assets/audio/beep.mp3"),d=0,Q={HELP:k,INFO:E,DIR:x,CLS:C,RAINY:L,AUDIO:G,SYSINFO:_,BASE64:H,BACK:P},h=0,g=[];function re(e){let[t]=e.target.value.split(" "),n=Q[t];if(n)n.run(e,Q);else{let a=`<div>'${t}' is not recognized as an internal or external command,</div>
+    <div>operable program or batch file.</div>
+    <div style="display: block; height: 20px;"></div>
+    `;document.getElementById("term_screen").innerHTML+=a;return}}function oe(e){let t=atob(e),n=new Uint8Array(t.length);for(let a=0;a<n.length;a++)n[a]=t.charCodeAt(a);return String.fromCharCode(...new Uint16Array(n.buffer))}function ie(e){if(d-=1,d==-1)d=g.length-1;e.target.value=g[d]}function ce(e){if(d+=1,d==g.length)d=0;e.target.value=g[d]}var me=(e)=>{if(e.code=="ArrowUp")return ie(e);if(e.code=="ArrowDown")return ce(e);if(e.code!=="Enter")return;let t=localStorage.getItem("/settings/audio/beep"),[n]=e.target.value.split(" ");if(!t&&n!=="BADAPPLE")M.play(),localStorage.setItem("/settings/audio/beep","yes");else if(t=="yes"&&n!=="BADAPPLE")M.play();let a=document.createElement("div");if(a.innerHTML=e.target.value,a.setAttribute("class","cmd_input"),e.target.parentNode.appendChild(a),e.target.parentNode.removeChild(e.target),e.target.removeEventListener("keydown",()=>{}),e.target.value.length!==0)g.push(e.target.value);if(h<10&&e.target.value.length==0)h+=1;if(h<10)re(e);else document.getElementById("term_screen").innerHTML=oe(Y),h=0;return document.getElementById("term_screen").innerHTML+=`<div style="display: flex; align-items: center;">
+    <div style="margin-right: 10px;">C:\\></div>
+    <input dummy="true" type="text" id="input_cmd" class="cmd_input" placeholder="Use 'HELP' to see more command!">
+  </div>`,document.getElementById("input_cmd").focus(),d=0,J()};function J(){document.getElementById("input_cmd").addEventListener("keydown",me)}J();
+
+//# debugId=26AFB026E2DA6DF064756E2164756E21
